26bb64c212add3ac24d4f9d97775191c3094088a
georginaxu <georginaxu@georginaxudeMacBook-Pro.local>
2018-04-17 09:28:42 -0400

    implement the function that only allows each student to make 1 friend at a time

--- socialize.m
+++ socialize.m
@@ -4,11 +4,17 @@ G2 = G;
 while t > 0
     for u = 1:size(G,1)
         prob_matrix = computeProbability(G2, u);
+        counter = 0;
         for v = 1:size(prob_matrix,2)
-            uv_prob = prob_matrix(1,v);
-            rand_p = rand;
-            if(rand_p <= uv_prob && G2(u,v) == 0)
-                G2(u,v) = 1; 
+            if(counter ~= 1)
+                uv_prob = prob_matrix(1,v);
+                rand_p = rand;
+                if(rand_p <= uv_prob && G2(u,v) == 0)
+                    G2(u,v) = 1; 
+                    counter = counter + 1;
+                end
+            else 
+                break;
             end
         end
     end

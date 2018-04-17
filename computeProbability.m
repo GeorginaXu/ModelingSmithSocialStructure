40e5a9a9bd979172b9368bdebf5f46bff2132d43
georginaxu <georginaxu@georginaxudeMacBook-Pro.local>
2018-04-17 01:48:35 -0400

    change the method to determine the probability

--- computeProbability.m
+++ computeProbability.m
@@ -1,13 +1,20 @@
 function[prob_matrix] = computeProbability(G,x)
 
 distance_matrix = computeDistance(G,x);
-prob_matrix = [];
+prob_matrix = zeros(1,size(distance_matrix,2));
 
-for d = 1:size(distance_matrix,1)
-    if(distance_matrix(1,d) == 0)
-        prob_matrix = [prob_matrix,0];
+for d = 1:size(distance_matrix,2)
+    if(distance_matrix(1,d) == 0 || G(x,d) == 1)
+        prob_matrix(1,d) = 0;
     else
-        p = 1/d;
-        prob_matrix = [prob_matrix, p];
+        D = distance_matrix(1, d);
+        if D == 1
+            p = 0.5;
+        elseif D == 2
+            p = 0.3;
+        else
+            p = 0.1;
+        end
+        prob_matrix(1,d) = p;
     end
 end

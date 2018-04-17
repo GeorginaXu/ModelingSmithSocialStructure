8094985af1e0f1cabf2bcfcf994d3348ef24038a
georginaxu <georginaxu@georginaxudeMacBook-Pro.local>
2018-04-17 01:49:24 -0400

    change the syntax error of finding the size of a vector

--- socialize.m
+++ socialize.m
@@ -4,7 +4,7 @@ G2 = G;
 while t > 0
     for u = 1:size(G,1)
         prob_matrix = computeProbability(G2, u);
-        for v = 1:size(prob_matrix,1)
+        for v = 1:size(prob_matrix,2)
             uv_prob = prob_matrix(1,v);
             rand_p = rand;
             if(rand_p <= uv_prob && G2(u,v) == 0)

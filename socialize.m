be380f713b340e80ec34e92340075a0fb205c0e7
georginaxu <georginaxu@georginaxudeMacBook-Pro.local>
2018-04-20 12:30:51 -0400

    add 4 extra input arguments

--- socialize.m
+++ socialize.m
@@ -1,7 +1,7 @@
 %a function that socializes graph G within t timesteps
 %for each node in G, it calculates the probability matrix and choose to
 %form 1 edge for each ndoe at 1 time
-function [G2] = socialize(G, t, method)
+function [G2] = socialize(G,S,J,P,F,t,method)
 
 if strcmp(method, "distance")
     %assign graph G to output G2
@@ -52,7 +52,7 @@ elseif strcmp(method, "classyear")
         %iterate over all nodes in G
         for u = 1:length(G)
             %compute the probability of u in graph G2
-            prob_matrix = computeProbability(G, u, "classyear");
+            prob_matrix = computeProbability(G, u, "classyear",S,J,P,F);
             if max(prob_matrix)>0
                 v = gendist(prob_matrix,1,1);
                 G2(u,v)=1;

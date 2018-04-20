39e3a738f2549b765307f3f2af07db342e6e2015
georginaxu <georginaxu@georginaxudeMacBook-Pro.local>
2018-04-20 12:30:25 -0400

    call socialize with 4 extra inputs

--- simulateHouse.m
+++ simulateHouse.m
@@ -7,20 +7,20 @@ function[G] = simulateHouse(S,J,P,F,t,method)
 
 size = S;
 G = zeros(S,S);  %start with G with S nodes and 0 edges
-G = socialize(G,t,method);     %let the S nodes socialize t times
+G = socialize(G,S,J,P,F,t,method);     %let the S nodes socialize t times
 
 G = [G; zeros(J,size)]; 
 size = size + J;
 G = [G(:,1:S) zeros(size,J)];    %add the extra J nodes to the graph G
-G = socialize(G,t,method);     %let the S+J nodes socialize t times
+G = socialize(G,S,J,P,F,t,method);     %let the S+J nodes socialize t times
 
 G = [G; zeros(P,size)];     
 size = size + P;
 G = [G(:,1:S+J) zeros(size,P)];   %add the extra P nodes to the graph G
-G = socialize(G,t,method);     %let the S+J+P nodes socialize t times
+G = socialize(G,S,J,P,F,t,method);     %let the S+J+P nodes socialize t times
 
 G = [G; zeros(F,size)]; 
 size = size + F;
 G = [G(:,1:S+J+P) zeros(size,F)];   %add the extra F nodes to the graph G
-G = socialize(G,t,method);     %let the S+J+P+F nodes socialize t times
+G = socialize(G,S,J,P,F,t,method);     %let the S+J+P+F nodes socialize t times
 
